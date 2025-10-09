9dcf833b85b9e937fb437bce25a89ab59bb48e46
Aydario <aydar.zh@mail.ru>
2025-10-10 00:09:19 +0300

    3.2-crud

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Product, Stock, StockProduct
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description']
+    search_fields = ['title', 'description']
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    list_display = ['id', 'address']
+    search_fields = ['address']
+
+@admin.register(StockProduct)
+class StockProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'stock', 'product', 'quantity', 'price']
+    list_filter = ['stock', 'product']
+    
